99a554cdc17ce22c56f623c7425dae461c1ddd85
CBovell <venujaperera12@gmail.com>
2023-04-02 14:20:55 -0400

    bug fixes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const { application } = require('express')
 const express = require('express')
 const app =  express()
 const mongoose = require('mongoose')
-require('dotenv').config
+require('dotenv').config()
 
 app.use(express.json())
 
-mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true})
- .then((res)=> 
- console.log(res),
+mongoose.connect(process.env.DB_STRING)
+ .then((result)=> 
+ console.log('connected'),
  app.listen(3000)
  )
  .catch((err) => console.log(err))
