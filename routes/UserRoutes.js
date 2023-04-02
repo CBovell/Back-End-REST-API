995b1f81888dc00118ec5869891d76243a60fead
CBovell <venujaperera12@gmail.com>
2023-04-02 18:51:06 -0400

    login endpoint progress

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,15 +1,40 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/Users')
+const bcrypt = require('bcrypt')
 
 
 router.post('/login', async (req, res)=>{
 
-    const user = await User.find(username = req.username)
+    const user = await User.find({username: req.username})
+
+    if(user==null){
+        res.status(400).send("User not found")
+    }
+
+    try{
+        if(bcrypt.compare(req.body.password, user.password)){
+            res.status(200).json({token:token}).send()
+        }
+        res.status(400).send("Error")
+
+    }catch{
+        res.status(500).send()
+    }
+
+
 
 })
 
 router.post('/signup', async (req, res)=>{
+
+    if(User.find({username:req.body.username}) != null){
+        res.status(400).send("Username already in use")
+    }
+
+    if(User.find({username:req.body.email}) != null){
+        res.status(400).send("Email already in use")
+    }
 
 
 
@@ -27,7 +52,7 @@ router.post('/signup', async (req, res)=>{
     
 
     } catch(err){
-        res.status(400).json({messege:err}).send()
+        res.status(500).json({messege:err}).send()
     }
     
 
