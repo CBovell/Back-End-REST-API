424061a8379e75844a222b231eac47fb1c9c813c
CBovell <venujaperera12@gmail.com>
2023-04-02 21:53:14 -0400

    password encryption and jwt complete

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/Users')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 
 router.post('/login', async (req, res)=>{
@@ -14,7 +16,8 @@ router.post('/login', async (req, res)=>{
 
     try{
         if(bcrypt.compare(req.body.password, user.password)){
-            res.status(200).json({token:token}).send()
+            const encryptedUser=jwt.sign(user, process.env.SECRET_KEY)
+            res.status(200).json({token:encryptedUser}).send()
         }
         res.status(400).send("Error")
 
@@ -36,27 +39,31 @@ router.post('/signup', async (req, res)=>{
         res.status(400).send("Email already in use")
     }
 
-
-
-    const user =new User({
-        username: req.body.username,
-        email: req.body.email,
-        password:String,
-        cdate: Date.now,
-        pictureURL:req.body.pictureURL,
-        admin:false
-    })
-
     try{
-        const messege = await user.save()
+        const salt = await bcrypt.genSalt()
+        const hashedPass = await bcrypt.hash(req.body.password, salt)
+        const user =new User({
+            username: req.body.username,
+            email: req.body.email,
+            password:hashedPass,
+            cdate: Date.now,
+            pictureURL:req.body.pictureURL,
+            admin:false
+        })
     
+        try{
+            const messege = await user.save()
+            const encryptedUser=jwt.sign(user, process.env.SECRET_KEY)
+            res.status(200).json({token:encryptedUser}).send()
+    
+        } catch(err){
+            res.status(500).json({messege:err}).send()
+        }
 
-    } catch(err){
-        res.status(500).json({messege:err}).send()
+    
+    }catch{
+       res.status(500).send() 
     }
-    
-
-    
 
 })
 
